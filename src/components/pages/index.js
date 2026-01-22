98f8e9bb9e4cb5f8d26defa053ea1489fe47d900
shankar <shankar.l5252@gmail.com>
2026-01-22 22:57:00 +0530

    feat: add new pages in index.js

---
+++ src/components/pages/index.js
@@ -0,0 +1,10 @@
+// Export all page content components
+export { default as EventsPageContent } from './EventsPageContent';
+export { default as SubmissionsPageContent } from './SubmissionsPageContent';
+export { default as ApprovalsPageContent } from './ApprovalsPageContent';
+export { default as StudentsPageContent } from './StudentsPageContent';
+export { default as FacultyPageContent } from './FacultyPageContent';
+export { default as SettingsPageContent } from './SettingsPageContent';
+export { default as DepartmentsPageContent } from './DepartmentsPageContent';
+export { default as ImportPageContent } from './ImportPageContent';
+export { default as ReportsPageContent } from './ReportsPageContent';
