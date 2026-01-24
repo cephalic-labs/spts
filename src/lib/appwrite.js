52bcb521196fdca12b2a97220c3104f8743a054c
Saumyajit P. <mail@saumyajit.dev>
2026-01-24 14:07:11 +0530

    feat: add node-appwrite dependency and configure Appwrite client with API key.

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -2,7 +2,8 @@ import { Client, Account, Databases, OAuthProvider, ID, Query } from "appwrite";
 
 const client = new Client()
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)
+  .setKey(process.env.NEXT_PUBLIC_APPWRITE_API_KEY);
 
 const account = new Account(client);
 const databases = new Databases(client);
